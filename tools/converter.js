9ede201e67e860a8486b0c960e3129a937f0f794
Tejasvi Nareddy <tejunareddy@gmail.com>
2018-02-14 23:30:40 -0800

    Add googling hostname

--- tools/converter.js
+++ tools/converter.js
@@ -2,8 +2,12 @@
 
 let fs = require('fs');
 let xml_lines = fs.readFileSync('../data/deals_02-13-2018.html').toString().split("\n");
+let google = require('google');
 
 const DEAL_URL = "https://card.discover.com/cardmembersvcs/deals/app/home";
+google.resultsPerPage = 25;
+
+let deals = [];
 
 for (let i = 0; i < xml_lines.length; i++) {
   let xml_line = xml_lines[i];
@@ -17,5 +21,47 @@ for (let i = 0; i < xml_lines.length; i++) {
     expiry_date: xml_line.match("class=\"date\">(.*)<\\/div>")[1]
   };
 
-  console.log(deal);
+  deals.push(deal);
+}
+
+let index = 0;
+googleSearch(deals[index].site_name);
+
+function googleSearch(input) {
+  console.log('Googling for', input);
+  google(input, function (err, res) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    if (res.links.length === 0) {
+      console.error('Wrong length for res.links', res.links.length);
+      return;
+    }
+
+    let keepGoing = true;
+    for (let i = 0; keepGoing; i++) {
+      let link = res.links[i].href;
+      console.log('Found link', link);
+      if (link === null) {
+        console.warn('next link...');
+        i++;
+      } else {
+        deals[index].hostname = link;
+        keepGoing = false;
+      }
+    }
+
+    index++;
+    if (index === deals.length) {
+      fixHostNames();
+    } else {
+      googleSearch(deals[index].site_name);
+    }
+  });
+}
+
+function fixHostNames() {
+  console.log(deals);
 }
