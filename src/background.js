d9c121fc1ed88ac18ce75ebe9fae1bb43a13fc9e
Tejasvi Nareddy <tejunareddy@gmail.com>
2018-02-15 23:25:53 -0800

    Use latest_data.json in chrome rules

--- src/background.js
+++ src/background.js
@@ -1,27 +1,57 @@
 // When the extension is installed or upgraded
 chrome.runtime.onInstalled.addListener(function() {
 
-  // Replace all rules
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
 
-    // With a new rule
-    chrome.declarativeContent.onPageChanged.addRules([
+    loadJSON(function(response) {
+      // Parse JSON string into object
+      let data = JSON.parse(response);
 
-      {
-        conditions: [
-          new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {
-              hostEquals: 'www.ae.com'
-            }
-          })
-        ],
-        actions: [
-          new chrome.declarativeContent.ShowPageAction()
-        ]
-      }
+      chrome.declarativeContent.onPageChanged.addRules([
+        formatDataIntoRule(data)
+      ]);
 
-    ]);
+    });
 
   });
 
 });
+
+
+function formatDataIntoRule(deals) {
+  let rule = {
+    conditions: [],
+    actions: [
+      new chrome.declarativeContent.ShowPageAction()
+    ]
+  };
+
+  deals.forEach(deal => {
+    let condition = makeCondition(deal.site_url);
+    rule.conditions.push(condition);
+  });
+
+  return rule;
+}
+
+function makeCondition(site_url) {
+  return  new chrome.declarativeContent.PageStateMatcher({
+    pageUrl: {
+      hostEquals: site_url
+    }
+  });
+}
+
+function loadJSON(callback) {
+
+  let xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open('GET', '../data/latest_data.json', true);
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState === 4 && xobj.status === "200") {
+      // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+      callback(xobj.responseText);
+    }
+  };
+  xobj.send(null);
+}
