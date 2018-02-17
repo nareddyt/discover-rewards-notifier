b4c57f7fa893254307109a7a3fc8d9153744c454
Tejasvi Nareddy <tejunareddy@gmail.com>
2018-02-17 15:46:58 -0800

    testing with handlebars

---
+++ src/templates.js
@@ -0,0 +1,10 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['deal'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var helper;
+
+  return "<h1>"
+    + container.escapeExpression(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"title","hash":{},"data":data}) : helper)))
+    + "</h1>\r\n";
+},"useData":true});
+})();
