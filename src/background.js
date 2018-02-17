aa2e1f23023e52ec123a6a4c92852fd4188fa413
Tejasvi Nareddy <tejunareddy@gmail.com>
2018-02-16 23:29:41 -0800

    Switch from declarativeContent to tabs and pageAction api to give better flexibility in displaying deals

--- src/background.js
+++ src/background.js
@@ -1,47 +1,94 @@
 
 const LATEST_DATA_PATH = '../data/latest_data.json';
+let deals = null;
+let enabledTabs = {};
 
-// When the extension is installed or upgraded
-chrome.runtime.onInstalled.addListener(function() {
-
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-
-    fetchJSONFile(LATEST_DATA_PATH, function(data){
-
-       chrome.declarativeContent.onPageChanged.addRules([
-        formatDataIntoRule(data)
-      ]);
-
-    });
-
-  });
-
+fetchJSONFile(LATEST_DATA_PATH, function (data) {
+  deals = data;
+  chrome.tabs.onRemoved.addListener(onTabRemoved);
+  chrome.tabs.onUpdated.addListener(onTabUpdated);
 });
 
-
-function formatDataIntoRule(deals) {
-  let rule = {
-    conditions: [],
-    actions: [
-      new chrome.declarativeContent.ShowPageAction()
-    ]
-  };
-
-  deals.forEach(deal => {
-    let condition = makeCondition(deal.site_url);
-    rule.conditions.push(condition);
-  });
-
-  return rule;
+function onTabRemoved(tabId, removeInfo) {
+  delete enabledTabs[tabId];
+  console.debug(enabledTabs);
 }
 
-function makeCondition(site_url) {
-  console.log('condition for', site_url);
-  return new chrome.declarativeContent.PageStateMatcher({
-    pageUrl: {
-      hostEquals: site_url.toLowerCase()
+function onTabUpdated(tabId, changeInfo, tab) {
+  if (!deals) {
+    // console.warn(tabId, 'Waiting for discover deals to load');
+    return;
+  }
+
+  if (!changeInfo.url) {
+    // console.debug(tabId, 'Something other than the URL changed');
+    return;
+  }
+
+  let new_url = changeInfo.url;
+  // console.log(tabId, 'New url:', new_url);
+
+  let deal = getDealForUrl(new_url);
+  if (!deal) {
+    // console.debug(tabId, new_url, 'has no deals');
+
+    // If tab is enabled
+    // Hide page icon
+    // Hide popup (title for now)
+    // Remove from enabled
+    if (enabledTabs[tabId]) {
+      console.info(tabId, 'Hiding page action');
+      chrome.pageAction.hide(tabId);
+
+      // console.info(tabId, 'Setting default title');
+      chrome.pageAction.setTitle({
+        tabId: tabId,
+        title: 'No Discover Deal for this page'
+      });
+
+      // console.info(tabId, 'Removing', tabId, 'from enabledTabs');
+      delete enabledTabs[tabId];
+      console.debug(enabledTabs);
+
+      return;
     }
+
+    return;
+  }
+
+  console.log(tabId, new_url, 'has deal', deal);
+
+  // Enable page action
+  // Set popup (title for now)
+  // Add to enabled
+  // console.info(tabId, 'Showing page action');
+  chrome.pageAction.show(tabId);
+
+  // console.info(tabId, 'Setting title');
+  chrome.pageAction.setTitle({
+    tabId: tabId,
+    title: deal.title
   });
+
+  // console.info(tabId, 'Adding', tabId, 'to enabledTabs');
+  enabledTabs[tabId] = deal;
+  console.debug(enabledTabs);
+}
+
+/**
+ * Returns the deal object or null
+ */
+function getDealForUrl(url) {
+  for (let i = 0; i < deals.length; i++) {
+    let deal = deals[i];
+    let deal_url = deal.site_url;
+
+    if (url.includes(deal_url)) {
+      return deal;
+    }
+  }
+
+  return null;
 }
 
 function fetchJSONFile(path, callback) {
