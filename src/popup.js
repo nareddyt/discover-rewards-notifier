14ab046361b76cf75caca89ee3f9d93e5df26aec
Tejasvi Nareddy <tejunareddy@gmail.com>
2018-02-22 22:43:17 -0800

    Better UI for deals

--- src/popup.js
+++ src/popup.js
@@ -26,6 +26,11 @@ function createHtml(items) {
     let type = item.type;
 
     if (type === 'deal') {
+      item.title = item.title.split(",")[0];
+      if (item.expiry_date === "Ongoing") {
+        item.expiry_date = "No Expiry Date"
+      }
+
       document.body.innerHTML += dealTemplate(item);
     } else if (type === 'cashback') {
       document.body.innerHTML += cashbackTemplate(item);
--- src/templates.js
+++ src/templates.js
@@ -18,10 +18,16 @@ templates['cashback'] = template({"1":function(container,depth0,helpers,partials
 templates['deal'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "<h1>"
+  return "<div class=\"item-box\">\r\n    <h1>"
+    + alias4(((helper = (helper = helpers.site_name || (depth0 != null ? depth0.site_name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"site_name","hash":{},"data":data}) : helper)))
+    + "</h1>\r\n    <hr/>\r\n    <h2>"
     + alias4(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper)))
-    + "</h1>\r\n\r\n<a href="
+    + "</h2>\r\n\r\n    <div class=\"img-container\">\r\n        <img src="
+    + alias4(((helper = (helper = helpers.img_src_url || (depth0 != null ? depth0.img_src_url : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"img_src_url","hash":{},"data":data}) : helper)))
+    + ">\r\n    </div>\r\n\r\n    <a class=\"button\" href="
     + alias4(((helper = (helper = helpers.deal_url || (depth0 != null ? depth0.deal_url : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"deal_url","hash":{},"data":data}) : helper)))
-    + " target=\"_blank\">Click here to view deal (you must login to Discover first)</a>\r\n\r\n<br/>\r\n<br/>";
+    + " target=\"_blank\">\r\n        Go to Deal\r\n    </a>\r\n\r\n    <br/>\r\n    You must login to <a href=\"https://www.discover.com\" target=\"_blank\">Discover.com</a> first.\r\n\r\n</div>\r\n\r\n<div class=\"expiry\">\r\n    "
+    + alias4(((helper = (helper = helpers.expiry_date || (depth0 != null ? depth0.expiry_date : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"expiry_date","hash":{},"data":data}) : helper)))
+    + "\r\n</div>\r\n\r\n<br/>\r\n<br/>";
 },"useData":true});
 })();
