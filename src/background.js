15fdf6cfe6e3289e16de305275ebe18ef8d24067
Tejasvi Nareddy <tejunareddy@gmail.com>
2018-03-25 12:44:16 -0700

    Comments on all scripts

--- src/background.js
+++ src/background.js
@@ -1,47 +1,73 @@
+/**
+ * This is the extension's background script. It always runs in the background.
+ * It is in charge of determining which deals or cashback rewards correspond to the current site the user is browsing.
+ * It does this by matching the tab's url to the urls in the data files.
+ */
 
 const CASHBACK_DATA_PATH = '../data/cashback/data.json';
 const DEAL_DATA_PATH = '../data/deal/data.json';
 
+// Loaded data from the files
 let cashbacks = null;
 let deals = null;
+
+// Dictionary that keeps track of the currently enabled "items" for each tab
+// Example: { 'tab 1': ['deal_1', 'deal_2', 'cashback_1'] }
 let enabledItems = {};
 
+// Load the cashback data into memory
 fetchJSONFile(CASHBACK_DATA_PATH, function (data) {
   cashbacks = data;
 
+  // Load the deals data into memory
   fetchJSONFile(DEAL_DATA_PATH, function (data) {
     deals = data;
+
+    // All the data is loaded into memory!
+    // Add event listeners for the tabs now. These actually do the matching and state changes
     chrome.tabs.onRemoved.addListener(onTabRemoved);
     chrome.tabs.onUpdated.addListener(onTabUpdated);
   });
 
 });
 
-function onTabRemoved(tabId, removeInfo) {
+/**
+ * Callback for when a tab is removed. Tied to the chrome event handler.
+ */
+function onTabRemoved(tabId) {
+  // Remove all enabled items for this tab
   delete enabledItems[tabId];
   console.debug(enabledItems);
 }
 
+/**
+ * Callback for when a tab is updated. Tied to the chrome event handler.
+ */
 function onTabUpdated(tabId, changeInfo, tab) {
-  // console.debug(tabId, changeInfo, tab);
+  console.debug(tabId, changeInfo, tab);
 
   if (!cashbacks || !deals) {
+    // Should never happen
     console.error(tabId, 'Discover deals or cashback rewards have not loaded properly');
     return;
   }
 
-  // Only check the tab's url if the page is loading
+  // If loading, then the url might not be populated yet.
+  // Note this doesn't correspond to the **page** loading. This is the **url** loading.
   if (changeInfo.status !== "loading") {
     return;
   }
 
-  // FIXME loading this each time might be inefficient. Could try caching the results
   let newUrl = tab.url;
-  // console.log(tabId, 'New url:', new_url);
+  console.log(tabId, 'New url:', newUrl);
 
+  // Determine which items should be displayed for this url
+  // FIXME loading this each time might be inefficient. Could try caching the results
   let items = getItemsForUrl(newUrl);
   if (items.length === 0) {
-    // console.debug(tabId, new_url, 'has no deals');
+    // There are no items for this url
+
+    console.debug(tabId, newUrl, 'has no deals');
 
     if (!getEnabledItems(tabId)) {
       // Page action for tab was already inactive, no need to do anything
@@ -57,17 +83,17 @@ function onTabUpdated(tabId, changeInfo, tab) {
     });
 
     // Remove from enabled
-    // console.info(tabId, 'Removing', tabId, 'from enabledItems');
+    console.info(tabId, 'Removing', tabId, 'from enabledItems');
     delete enabledItems[tabId];
     console.debug(enabledItems);
 
     return;
   }
-
+  // else, there are items for this page!
   console.log(tabId, newUrl, 'has items', items);
 
   // Enable page action and set title
-  // console.info(tabId, 'Showing page action');
+  console.info(tabId, 'Showing page action');
   chrome.pageAction.show(tabId);
   chrome.pageAction.setTitle({
     tabId: tabId,
@@ -75,31 +101,35 @@ function onTabUpdated(tabId, changeInfo, tab) {
   });
 
   // Add to enabled
-  // console.info(tabId, 'Adding', tabId, 'to enabledItems');
+  console.info(tabId, 'Adding', tabId, 'to enabledItems');
   enabledItems[tabId] = items;
   console.debug(enabledItems);
 }
 
 /**
- * Returns the deal and cashback objects that match the url
+ * Returns the deal and cashback objects that match the given url.
  */
 function getItemsForUrl(url) {
   let items = [];
 
+  // Iterate through deals
   for (let i = 0; i < deals.length; i++) {
     let deal = deals[i];
     let deal_url = deal.site_url;
 
+    // Simple sub-stringing to match urls with hostname
     if (url.includes(deal_url)) {
       deal.type = 'deal';
       items.push(deal);
     }
   }
 
+  // Iterate through cashbacks
   for (let i = 0; i < cashbacks.length; i++) {
     let cashback = cashbacks[i];
     let cashback_url = cashback.site_url;
 
+    // Simple sub-stringing to match urls with hostname
     if (url.includes(cashback_url)) {
       cashback.type = 'cashback';
       items.push(cashback);
@@ -109,6 +139,9 @@ function getItemsForUrl(url) {
   return items;
 }
 
+/**
+ * Loads a json file into memory. Returns the object
+ */
 function fetchJSONFile(path, callback) {
   let httpRequest = new XMLHttpRequest();
   httpRequest.onreadystatechange = function() {
@@ -123,6 +156,10 @@ function fetchJSONFile(path, callback) {
   httpRequest.send();
 }
 
+/**
+ * Gets the enabled items for a tab.
+ * Note this is used across files! Popup.js calls this.
+ */
 function getEnabledItems(tabId) {
   return enabledItems[tabId];
 }
--- src/popup.js
+++ src/popup.js
@@ -1,57 +1,86 @@
+/**
+ * This is the extension's popup script. It is responsible for displaying the correct items in the popup.
+ * This script run every time the extension icon is clicked (every time the popup is opened).
+ */
 
+/**
+ * Determine which items to show in the popup for the current tab
+ */
 function fetchDeal() {
 
+  // Create a query to get the current tab the popup is for
   let query = {
     active: true,
     currentWindow: true
   };
 
+  // Run the query
   chrome.tabs.query(query, function (tabs) {
+
+    // Get the current tab id
     let tab = tabs[0];
     let tabId = tab.id;
     console.info('Popup for tab', tabId);
 
+    // Get the enabled items for this tab.
+    // This is all stored in the background page. See background.js
     let enabledItems = chrome.extension.getBackgroundPage().getEnabledItems(tabId);
     console.debug('Popup got', enabledItems);
 
+    // Create the html with these items
     createHtml(enabledItems);
   });
 }
 
+/**
+ * Creates the html for the items passed in. Then adds it to the DOM.
+ */
 function createHtml(items) {
+  // Fetch handlebar templates for the deal and cashback
+  // FIXME we don't need to load this in every time, move it out of this scope
   let dealTemplate = Handlebars.templates['deal'];
   let cashbackTemplate = Handlebars.templates['cashback'];
 
+  // Temp array of html strings
   let htmls = [];
 
   items.forEach(function (item) {
+    let html = "<div>Something went wrong!</div>";
+
+    // Create the correct html based on the item type
     let type = item.type;
-
-    let html = "<div></div>";
-
     if (type === 'deal') {
+
+      // We need to change the expiry date for deals (a little)
       item.title = item.title.split(",")[0];
       if (item.expiry_date === "Ongoing") {
         item.expiry_date = "No Expiry Date"
       }
 
+      // Create the html for the deal via the handlebars template
       html = dealTemplate(item);
+
     } else if (type === 'cashback') {
+      // Create the html for the cashback reward via the handlebars template
       html = cashbackTemplate(item);
+
     } else {
+      // This should never happen
       console.error('Unsupported type', type);
     }
 
+    // Add the html to our temp array
     htmls.push(html);
   });
 
+  // Add all html objects to the DOM
   let obj = {
     items: htmls
   };
-
   let mainTemplate = Handlebars.templates['main'];
   document.body.innerHTML += mainTemplate(obj);
 
 }
 
+// Whenever the script starts, run this function
 window.onload = fetchDeal;
