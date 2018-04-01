b03bb3f9de8c2e234b79a0a65436dc2842849703
Tejasvi Nareddy <tejunareddy@gmail.com>
2018-03-31 18:30:17 -0700

    Loading and sending of json data

--- backend/app.js
+++ backend/app.js
@@ -4,22 +4,37 @@
  * @author nareddyt
  */
 
+const DEALS_JSON_FILE = '../data/deal/data.json';
+const CASHBACKS_JSON_FILE = '../data/cashback/data.json';
+
+const deals_data = require(DEALS_JSON_FILE);
+const cashback_data = require(CASHBACKS_JSON_FILE);
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-app.use(bodyParser.json());
 const port = process.env.PORT || 5000;
 
+app.use(bodyParser.json());
+
 app.get('/', function (req, res) {
-    console.log('info', req.originalUrl + ' with payload ' + JSON.stringify(req.body) + ' and headers ' + JSON.stringify(req.headers));
-    res.send('Hello World! See this link for more info: https://www.tejunareddy.com/discover-rewards-notifier/');
+  console.log('info', req.originalUrl + ' with payload ' + JSON.stringify(req.body) + ' and headers ' + JSON.stringify(req.headers));
+  res.send('Hello World! See this link for more info: https://www.tejunareddy.com/discover-rewards-notifier/');
 });
 
 app.get('/ping', function (req, res) {
-    res.send('pong');
+  res.send('pong');
+});
+
+app.get('/deals', function (req, res) {
+  res.json(deals_data);
+});
+
+app.get('/cashbacks', function (req, res) {
+  res.json(cashback_data);
 });
 
 app.listen(port, function () {
-    console.warn('Listening on port', port)
+  console.warn('Listening on port', port)
 });
