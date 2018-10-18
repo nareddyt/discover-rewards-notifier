e45817b67b0dfbefd64e266e0bb887f2d781d463
Tejasvi (Teju) Nareddy <tejunareddy@gmail.com>
2018-10-18 19:52:54 -0400

    removing whitespace
    Co-Authored-By: MikeM711 <michaelmccabe711@gmail.com>

--- tools/dataProcessor.js
+++ tools/dataProcessor.js
@@ -125,7 +125,7 @@ function parseCashbacks() {
         let suffix_check = ['.com',' - Special',' - Featured','®','™' ]
 
         for(let i = 0; i < suffix_check.length; i++){
-          if (cashback.site_name .indexOf(suffix_check[i]) != -1 && cashback.site_name != 'Hotels.com') {
+          if (cashback.site_name.indexOf(suffix_check[i]) != -1 && cashback.site_name != 'Hotels.com') {
             cashback.site_name  = cashback.site_name .replace(suffix_check[i],'');
           }
       }
