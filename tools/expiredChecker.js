bfce9eeefe152d5ca62cbe0bd8f9a90537ee83a6
jmoore914 <jmoore914@gmail.com>
2018-10-19 14:09:35 -0400

    Changed varaible name from deal to item; exception now prints file name; added task to CircleCI

--- tools/expiredChecker.js
+++ tools/expiredChecker.js
@@ -13,14 +13,14 @@ function checkExpiredAll() {
 function checkExpiredFile(file) {
   let json = loadJSON(file);
   let today = new Date();
-  json.forEach(function(deal) {
-    if (deal.hasOwnProperty('expiry_date')) {
-      let expiry = deal.expiry_date;
+  json.forEach(function(item) {
+    if (item.hasOwnProperty('expiry_date')) {
+      let expiry = item.expiry_date;
       if (expiry.indexOf('Expires ') !== -1) {
         let dateArray = expiry.replace('Expires ', '').split('/');
         let expiryDate = new Date(dateArray[2], dateArray[0], dateArray[1]);
         if (expiryDate < today) {
-          throw deal.title + ',' + deal.expiry_date;
+          throw file + ', ' + item.title + ', ' + item.expiry_date;
         }
       }
     }
